5c35b432f8018be498a7ea2ff393ab260cb9f65b
Nilo-q <daquintero891@misena.edu.co>
2023-10-14 11:51:33 -0500

    Actualizacion 14/10/2023

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomersModule } from './customers/customers.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, AuthModule, CustomersModule, InventoryModule, OrdersModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
